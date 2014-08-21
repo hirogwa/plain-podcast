04a8186f764631a55c0295c0152d5f5e42cd914d
hirogwa <hirogwa@gmail.com>
2014-08-20 23:34:55 -0700

    fix twitter url reference.

--- views.py
+++ views.py
@@ -4,30 +4,31 @@ from models import Episode, Podcast, Statement, Presenter
 
 def index(request):
     context = {'episodes': Episode.objects.all().order_by('-pub_date')}
-    context.update(get_common_context())
+    context.update(get_common_context(request))
     return render(request, 'podcast/index.html', context)
 
 
 def about(request):
     presenters = Presenter.objects.all().order_by('display_order')
     context = {'presenters': presenters}
-    context.update(get_common_context())
+    context.update(get_common_context(request))
     return render(request, 'podcast/about.html', context)
 
 
 def episode(request, slug):
     episode_obj = get_object_or_404(Episode, slug=slug)
     context = {'episode': episode_obj}
-    context.update(get_common_context())
+    context.update(get_common_context(request))
     return render(request, 'podcast/episode.html', context)
 
 
-def get_common_context():
+def get_common_context(request):
     podcast_obj = get_object_or_404(Podcast, pk=1)
     footer_about = Statement.objects.filter(unique_name='footer-about').first()
     footer_twitter = Statement.objects.filter(unique_name='footer-twitter').first()
     footer_subscription = Statement.objects.filter(unique_name='footer-subscription').first()
-    context = {'podcast': podcast_obj,
+    context = {'host': request.META['HTTP_HOST'],
+               'podcast': podcast_obj,
                'footer_about': footer_about,
                'footer_twitter': footer_twitter,
                'footer_subscription': footer_subscription,
