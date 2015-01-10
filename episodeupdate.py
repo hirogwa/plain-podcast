a01024adf0a5c5b04b7a2f6aa4d87e4d2ce6765c
hirogwa <hirogwa@furano.local>
2015-01-09 21:39:16 -0800

    fix #11

--- episodeupdate.py
+++ episodeupdate.py
@@ -1,15 +1,17 @@
 import yaml
 import sys
 
-configfile = file('podcast.yaml', 'r')
+configfile = file('plainpodcast.yaml', 'r')
 config = yaml.load(configfile)
 sys.path.append(config.get('project_directory'))
 
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', config.get('project_settings'))
 
-from django.core.files.base import ContentFile
-from models import ScheduledEpisode, Episode
+import django
+django.setup()
+
+from plainpodcast.models import ScheduledEpisode, Episode
 import datetime
 import logging
 import pytz
@@ -19,7 +21,8 @@ logger = logging.getLogger(__name__)
 
 handler = logging.FileHandler('batch.log')
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s(%(name)s)%(levelname)s: %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s(%(name)s)%(levelname)s: %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -31,7 +34,8 @@ class EpisodeUploader():
 
     def execute_all(self):
         logger.info('checking for episodes to upload.')
-        scheduled_episodes = ScheduledEpisode.objects.all().order_by('pub_date')
+        scheduled_episodes = ScheduledEpisode.objects.all().order_by(
+            'pub_date')
         uploaded_episodes = []
         for ep in scheduled_episodes:
             ep_time = ep.pub_date
@@ -49,13 +53,9 @@ class EpisodeUploader():
                               slug=episode.slug,
                               description=episode.description,
                               show_notes=episode.show_notes,
+                              audio_file=episode.audio_file,
                               pub_date=episode.pub_date)
-
-        audio_copied = ContentFile(episode.audio_file.read())
-        new_episode.audio_file.save(episode.audio_file.name, audio_copied)
         new_episode.save()
-
-        episode.audio_file.close()
 
 
 if __name__ == '__main__':
